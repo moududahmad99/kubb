6fe8bc8612036c52d98c810238c620a74338c9ec
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2022-11-17 11:10:44 +0600

    Kubbs-first-committ

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#banner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<i class="fa-solid fa-arrow-left slick-arrow1"></i>',
+        nextArrow: '<i class="fa-solid fa-arrow-right slick-arrow2"></i>'
+      });
+})
