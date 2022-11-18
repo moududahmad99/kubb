2a49064bac2655721df91605a7d6a3f3dd6827e1
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2022-11-18 12:03:41 +0600

    complete banner and nav section without responsive

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
-$(function() {
-    $('#banner').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<i class="fa-solid fa-arrow-left slick-arrow1"></i>',
-        nextArrow: '<i class="fa-solid fa-arrow-right slick-arrow2"></i>'
-      });
-})
+// $(function() {
+//     $('#banner').slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         prevArrow: '<i class="fa-solid fa-arrow-left slick-arrow1"></i>',
+//         nextArrow: '<i class="fa-solid fa-arrow-right slick-arrow2"></i>'
+//       });
+// })
